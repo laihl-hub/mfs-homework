cfcc87b22db9ce4f5ce5782c25f4f81d67c80507
laihl <1132281154@qq.com>
2021-12-14 17:44:03 +0800

    单文件组件

---
+++ 突破/vue学习/vue-start/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <span>{{ total }}</span>
+    <!-- <router-view/> -->
+    <!-- <hello-world /> -->
+    <myBtn @btnChange="sum"></myBtn>
+    <myBtn @btnChange="sum"></myBtn>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import myBtn from "./components/my-btn.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      total: 0,
+    };
+  },
+  methods: {
+    sum() {
+      this.total++;
+    },
+  },
+  components: { myBtn },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+span {
+  display: block;
+  margin-bottom: 20px;
+  font-size: 30px;
+}
+</style>
---
+++ 突破/vue学习/vue-start/src/components/my-btn.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="add">{{ counter }}</button>
+</template>
+
+<script>
+export default {
+  name: "myBtn",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.counter++;
+      this.$emit("btnChange");
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  width: 50px;
+  height: 30px;
+  background-color: rgb(218, 236, 247);
+  margin-left: 3px;
+  border-radius: 3px;
+}
+</style>
